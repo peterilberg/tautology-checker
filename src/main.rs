553c5817d6ab1ea0a53d0646944923ee2d9a4eb7
Peter Ilberg <peter.ilberg@gmail.com>
2026-03-09 14:41:01 +0100

    Refactor main program.

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,35 @@ use prop::Prop;
 use std::{env, str::FromStr};
 
 fn main() {
-    let mut show_usage = false;
-    let mut print_newline = false;
-
     let args: Vec<String> = env::args().collect();
-    for arg in args.iter().skip(1) {
-        if print_newline {
-            println!();
-        }
-        print_newline = true;
-
-        let prop = Prop::from_str(arg);
-        println!("text: {}", arg);
-        match prop {
-            Ok(prop) => {
-                println!("prop: {}", prop);
-                println!("taut: {}", prop.is_tautology());
-            }
-            Err(prop::Error { position, message }) => {
-                println!("pos : {:>width$}", "^", width = position + 1);
-                println!("err : {}", message);
-                show_usage = true;
-            }
-        }
-    }
-
-    if show_usage {
+    if process_all(&args[1..]).is_err() {
         println!();
         usage(&args[0]);
+    }
+}
+
+fn process_all(propositions: &[String]) -> Result<(), ()> {
+    process_one(&propositions[0])?;
+    for proposition in &propositions[1..] {
+        println!();
+        process_one(proposition)?;
+    }
+    Ok(())
+}
+
+fn process_one(proposition: &String) -> Result<(), ()> {
+    println!("text: {}", proposition);
+    match Prop::from_str(proposition) {
+        Ok(prop) => {
+            println!("prop: {}", prop);
+            println!("taut: {}", prop.is_tautology());
+            Ok(())
+        }
+        Err(prop::Error { position, message }) => {
+            println!("pos : {:>width$}", "^", width = position + 1);
+            println!("err : {}", message);
+            Err(())
+        }
     }
 }
 
